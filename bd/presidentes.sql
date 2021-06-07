63160b9ef7d4d127bee81d10c52ac0de5c4a4a1f
PablitoElMaldy <pablojamiro2008@hotmail.com>
2021-06-06 21:52:00 -0500

    bd con postgree agregada

---
+++ bd/presidentes.sql
@@ -0,0 +1,28 @@
+create table tb_presidente(
+	"id_pre" int not null,
+	"dni_pre" varchar(8) not null,
+	"nom_pre" varchar(40) not null,
+	"apepat_pre" varchar(25) not null,
+	"apemat_pre" varchar(25) not null,
+	primary key ("id_pre")
+);
+
+
+insert into tb_presidente values (1, '10001088', 'Keiko Sofia', 'Fujimori', 'Higuchi');
+insert into tb_presidente values (2, '01211014', 'Yonhy', 'Lescano', 'Ancieta');
+insert into tb_presidente values (3, '17903382', 'Cesar', 'Acuña', 'Peralta');
+insert into tb_presidente values (4, '09155583', 'Hernando', 'De Soto', 'Polar');
+insert into tb_presidente values (5, '19669215', 'Andres Avelino', 'Alcantara', 'Paredes');
+insert into tb_presidente values (6, '26605193', 'Marco Antonio', 'Arana', 'Zegarra');
+insert into tb_presidente values (7, '40765768', 'Veronica Fanny', 'Mendoza', 'Frisch');
+insert into tb_presidente values (8, '18010708', 'Daniel Enrique', 'Salaverry', 'Villa');
+insert into tb_presidente values (9, '09337130', 'Julio Armando', 'Guzman', 'Caceres');
+insert into tb_presidente values (10,'44123390', 'Ollanta Moises', 'Humala', 'Tasso');
+insert into tb_presidente values (11,'06630665', 'Alberto Ismael', 'Beingolea', 'Delgado');
+insert into tb_presidente values (12,'27427864', 'Jose Pedro', 'Castillo', 'Terrones');
+insert into tb_presidente values (13,'08274605', 'Rafael Gaton Tadeo Milagros', 'Santos', 'Normand');
+insert into tb_presidente values (14,'43863835', 'Daniel Belizario', 'Urresti', 'Elera');
+insert into tb_presidente values (15,'19813153', 'Ciro Alfredo', 'Galvez', 'herrera');
+insert into tb_presidente values (16,'07845838', 'Rafael Bernardo', 'Lopez Aliaga', 'Carzola');
+insert into tb_presidente values (17,'08330968', 'Jose Alejandro', 'Vega', 'Antonio');
+insert into tb_presidente values (18,'41265978', 'George Patrick', 'Forsyth', 'Sommer');
